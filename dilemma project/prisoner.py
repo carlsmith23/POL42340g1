46abcf9ecda32fceccd123ab5b4250eec0b1cfe6
Carl Smith <carl.smith@ucdconnect.ie>
2023-03-07 14:54:44 +0000

    Lab5 working

--- dilemma project/prisoner.py
+++ dilemma project/prisoner.py
@@ -1,67 +1,96 @@
 import random
 
+class prisoner
+    def __init__(self, name, prisonerNumber, number_of_colors, color, crime, strategy, imprisoned, sentence):
+        self.name = first_name[fname] + " " + last_name
+        self.prisonerNumber = pnumber
+        self.color = color[pcolor]
+        self.strategy = strategy[pstrat]
+        self.imprisoned = True
+        self.sentence = sentence
 
-first_name = ["Archie", 
-                "Billy",
-                "Charlie",
-                "Donnie",  
-                "Eddie",
-                "Frankie",
-                "Gerry",
-                "Harry",
-                "Izzy",
-                "Johnny",
-                "Kenny",
-                "Lenny",
-                "Manny",
-                "Normie",
-                "Ozzie",
-                "Patty",
-                "Q",
-                "Rusty",
-                "Sammy",
-                "Timmy",
-                "U",
-                "Vinnie",
-                "Willy",
-                "X",
-                "Y",
-                "Z", 
-                ]
+def generate_name():
+    first_name = ["Archie", 
+                    "Billy",
+                    "Charlie",
+                    "Donnie",  
+                    "Eddie",
+                    "Frankie",
+                    "Gerry",
+                    "Harry",
+                    "Izzy",
+                    "Johnny",
+                    "Kenny",
+                    "Lenny",
+                    "Manny",
+                    "Normie",
+                    "Ozzie",
+                    "Patty",
+                    "Q",
+                    "Rusty",
+                    "Sammy",
+                    "Timmy",
+                    "U",
+                    "Vinnie",
+                    "Willy",
+                    "X",
+                    "Y",
+                    "Z", 
+                    ]
 
-last_name = ["Angry Eyes",
-                "Bonecrusher",
-                "the Crime Doer",
-                "Da Kid",
-                "Eggbreath",
-                "Fancypants",
-                "Goofballs",
-                "the Horse",
-                "I",
-                "J",
-                "K",
-                "L",
-                "Moneybags",
-                "N",
-                "One Eye",
-                "the Professor",
-                "Q",
-                "R",
-                "S",
-                "Tightlips",
-                "the Usurper",
-                "Vicious",
-                "Wonderful",
-                "X",
-                "Y",
-                "Zero"
-                ]
-
-i = 0
-reps = 100
-while i < reps:
+    last_name = ["Angry Eyes",
+                    "Bonecrusher",
+                    "the Crime Doer",
+                    "Da Kid",
+                    "Eggbreath",
+                    "Fancypants",
+                    "Goofballs",
+                    "the Horse",
+                    "I",
+                    "J",
+                    "K",
+                    "L",
+                    "Moneybags",
+                    "N",
+                    "One Eye",
+                    "the Professor",
+                    "Q",
+                    "R",
+                    "S",
+                    "Tightlips",
+                    "the Usurper",
+                    "Vicious",
+                    "Wonderful",
+                    "X",
+                    "Y",
+                    "Zero"
+                    ]
     fname = random.randint(0,25)
     lname = random.randint(0,25)
-    print(first_name[fname] + " " + last_name[lname])
-    i += 1
-  
+    name = first_name[fname] + " " + last_name[lname]
+    return name
+
+def set_prisoner_number():
+    return pnumber    
+
+def set_random_color(number_of_colors):
+    colors = ["Red",
+                "Blue,
+                "Yellow",
+                "Green",
+                "Orange",
+                "Purple"
+                ]
+    color = random.randint(0, number_of_colors)
+    return colors[color]
+
+def set_strategy():
+    strategies = ["Altruist", 
+                    "Egoist", "Ethnocentrist", "Cosmopolitan"
+                    ]
+        strategy = random.randint(0,3)
+        return strategies[stategy]
+        
+
+
+
--- lab5.py
+++ lab5.py
@@ -23,13 +23,17 @@
 
 #1b. Implement the quit functionality: if the user presses q or Q, it should exit the menu loop (and thus the program).
 
-   
+import random
 
-
-#1c. Implement the map size functionality. If the user presses s or S, ask for the size of the map. Write a separate function for this.
-#2a. Expand the function written in 1c to not accept anything other than a number, and only a number between 1 and 100.
+run = 1
+map_size = 0
+map_max_size = 100
+map = []
+agents = 0
+b = "z"   
 
 def set_map_size(map_max_size):
+    valid_input = 0
     while valid_input == 0:
         map_size = input("How big do you want the map to be? (1-{}): " .format(map_max_size))
         if map_size.isnumeric:
@@ -42,6 +46,7 @@ def set_map_size(map_max_size):
             print("Error: Must be a number")
     return map_size
 
+
 def generate_map(map_size): 
     map = []
     list_length = 0
@@ -52,30 +57,18 @@ def generate_map(map_size):
         map.append(value)
     return map
 
-def get_map_index(x, y, map_size, list):
-    index = (x-1) * map_size + (y-1)
-    return(index)
-
-def get_map_value(x, y, map_size, list):
-    index = (x-1) * map_size + (y-1)
-    return(list[index])
-
-
-
-
-#1d. Implement the agent number functionality. Write a separate function for this.
-#2b. Expand the function written in 1d to only work when the size of the map is set and to not accept anything other than a number, and only a number between 1 and the number of cells. Since it is a square map, the number of cells is N * N, N being the length of one side of the square, as entered in 1e.
 
 def how_many_agents(map_size, map):
+    valid_input = 0
     if map_size == 0:
         print("Set map size first")
         return
     else:
         while valid_input == 0:
-            agents = input("How many agents do you want on the map?")
+            agents = input("How many agents do you want on the map? ")
             if agents.isnumeric:
                 agents = int(agents)
-                if agents <= len(map):
+                if agents >= len(map):
                     valid_input = 1
                 else:
                     print("Error: The map isn't big enough to hold that many agents!")    
@@ -84,61 +77,45 @@ def how_many_agents(map_size, map):
         return agents
 
 
+def get_map_index(x, y, map_size, list):
+    index = (x-1) * map_size + (y-1)
+    return(index)
 
-
-#3a. Update the function written in 1a, so that after the user sets the map size (2a), a map is automatically generated (use 2a from Lab 4).
-
-
-
-
-
-
-#3b. Update the function written in 1a, so that after the user sets the number of agents (2b), the map is updated so that all agents are added (use 4 from Lab 4).
 
 def place_agents(map_size, map, agents):
-    for i in agents:
+    for i in range(agents):
+        success = 0
         while success == 0:
             x = random.randint(1, map_size)
             y = random.randint(1, map_size)
-            value = get_list_value(x, y, map_size, map)
-            if value == 1:
-                pass
-            else:
-                index = get_list_index(x, y, map_size, map)
+            index = get_map_index(x, y, map_size, map)
+            if map[index] == 0:
+                index = get_map_index(x, y, map_size, map)
                 map[index] = 1
                 success = 1
     return(map)
 
 
-
-#4. Write a function to print the map, where zeroes are represented by "-" and ones by "X".
-
 def print_map(map_size, map):
-    for i in map:
-        if map[i]:
-            map[i] = "X"
-        else:
-            map[i] = "-"
     col = 0
     row = 0
     x = 0
     for row in range(map_size):
         for col in range(map_size):
-            print(map[(col+x)], end="")    
+            if map[(col+x)] == 1:
+                print("X", end="")
+            else:
+                print("-", end="")
         x = x + map_size
         print("")
+  
 
 
 
 
 
 
-#Main Program Loop
-run = 1
-map_size = 0
-map_max_size = 100
-map = []
-agents = 0
+#########Main Program Loop###########
 
 while run == 1:
     print("Set (s)ize of the map")
@@ -146,20 +123,36 @@ while run == 1:
     print("(G)enerate map")
     print("(P)rint map")
     print("(Q)uit")
-    buffer = input("Select an option: ")
-    if buffer == "q" or "Q":
-        run = 0
-    elif buffer == "s" or "S":
-        map_size = set_map_size(map_max_size)
-    elif buffer == "a" or "A":    
-        agents = how_many_agents(map_size)
-        map = generate_map(map_size)
-        print_map(map_size, map)    
-    elif buffer == "g" or "G":
-        map = generate_map(map_size)
-        print_map(map_size, map)
-    elif buffer == "p" or "P":
-        print_map(map_size, map)
+    print("")
+    b = input("Select an option: ")
+    print("")
+    if b.isalpha and len(b) == 1:
+        if b == "s" or b == "S":
+            map_size = set_map_size(map_max_size)
+            print("")
+            print("Map size set to {}" .format(map_size))
+            print("")
+            print("")    
+        elif b == "a" or b == "A":
+            agents = how_many_agents(map_size, map)
+            print("")
+        elif b == "g" or b == "G":
+            map = generate_map(map_size)
+            print("")
+            print("Map generated...")
+            map = place_agents(map_size, map, agents)
+            print("")
+            print("Agents placed")
+            print_map(map_size, map)
+            print("") 
+        elif b == "p" or b == "P":
+            print_map(map_size, map)
+            print("")
+        elif b == "q" or b == "Q":
+            run = 0
+        else:
+            print("Error: Not a valid selection")
+            print("")
     else:
-        print("Error: Choose a valid option")
-        print("")   
+        print("Error: not a valid selection")
+        print("")
